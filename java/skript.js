8b0e2136f73fe09a4262138f8e5d57b95b7e8da2
Yeardear0812 <yeardear18@gmail.com>
2026-04-10 11:52:20 +0300

    k

--- java/skript.js
+++ java/skript.js
@@ -1,7 +1,12 @@
 const closeBtn = document.querySelector('#closeBtn');
+const openBtn = document.querySelector('#openBtn');
 const form = document.querySelector('.fixed-form');
 
 closeBtn.addEventListener('click', function(e) {
     e.preventDefault();
     form.style.display = 'none';
+});
+
+openBtn.addEventListener('click', function() {
+    form.style.display = 'block';
 });
